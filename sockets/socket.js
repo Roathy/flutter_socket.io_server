f68392bc337bf32a4a6690dba9de8bb60c101d3c
Roathy VM <gogczoro@gmail.com>
2022-06-21 19:20:12 -0500

    socketed server with basic business logic

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,16 +1,46 @@
-const {io} = require('../index');
+const { io } = require('../index');
+
+const Bands = require('../models/bands_model');
+const Band = require('../models/band_model');
+
+const bands = new Bands();
+
+bands.addBand(new Band('falling in reverse'));
+bands.addBand(new Band('blind guardian'));
+bands.addBand(new Band('ayreon'));
+bands.addBand(new Band('system of a down'));
+bands.addBand(new Band('bring me the horizon'));
 
 //Sockets messages
 io.on('connection', client => { //client=dispositivo que se conectó al server
   console.log('cliente conectado');
-  //client.on('event', data => {});
+
   client.on('disconnect', () => {
     console.log('cliente desconectado');
   });
 
   client.on('clientMessage', (payload) => {
-    console.log('got one message: ', payload.name)
+    console.log('got one message: ', payload.name);
   });
 
-  io.emit('serverMessage', { message: 'all clients listen!' });
+  client.on('new-message', (payload) => {
+    client.broadcast.emit('new-message', payload);
+  });
+
+  client.emit('current-bands', bands.getBands());
+
+  client.on('add-new-band', (payload) => {
+    bands.addBand(new Band(payload.name));
+    io.emit('current-bands', bands.getBands());
+  });
+
+  client.on('delete-band', (payload) => {
+    bands.deleteBand(payload.id);
+    io.emit('current-bands', bands.getBands());
+  });
+
+  client.on('vote-for-band', (payload) => {
+    bands.voteBand(payload.id);
+    io.emit('current-bands', bands.getBands());
+  });
 });
